c28596e7dba6e61bfc18d763427e010c441e9866
azizx018 <chiara.aziz@gmail.com>
2022-05-31 13:11:49 -0500

    added UserAccountRepository file

---
+++ user/src/main/java/net/yorksolutions/users/UserAccountRepository.java
@@ -0,0 +1,14 @@
+package net.yorksolutions.users;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserAccountRepository extends CrudRepository<UserAccount, Long> {
+
+    Optional<UserAccount> findByUsernameAndPassword(String username, String password);
+
+    Optional<UserAccount> findByUsername(String username);
+}
